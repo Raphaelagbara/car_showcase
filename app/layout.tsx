ded4118f06e76b2a79a1534e3839e6a2b3f72d20
Raphael Agbara <ralphagbara@email.com>
2023-07-15 11:03:00 +0100

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import './globals.css'
+
+import {Navbar,Footer} from '@/components';
+
+
+export const metadata = {
+  title: 'Car Hub Service',
+  description: 'Discover the best cars in the world',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className="relative
+      ">
+        <Navbar/>
+        {children}
+        <Footer/>
+      </body>
+    </html>
+  )
+}
